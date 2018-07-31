2e1f84b30dcc8c19cee8bac2127e6f2424f72d6c
Standard <Standard@Erics-MacBook-Air.local>
2018-07-31 10:35:37 -0400

    Fix bugs

--- src/main/java/com/dnastack/dos/server/config/SecurityConfig.java
+++ src/main/java/com/dnastack/dos/server/config/SecurityConfig.java
@@ -43,6 +43,8 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		super.configure(http);
+		
+		http.csrf().disable();
 		http.authorizeRequests()
 			.antMatchers("/noneSecurity")
 			.hasRole("user")
